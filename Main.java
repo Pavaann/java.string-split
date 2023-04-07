8a8ce1bf68495badd8df6418a07fb752bfd20922
Pavaann <pavanrafaela18@gmail.com>
2023-04-07 15:00:23 -0300

    first commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+package course;
+public class Main {
+    public static void main(String[] args) {
+
+        String s = "potato apple lemon banana cherry avocado pear watermelon";
+        String[] vect = s.split(" ");
+
+        System.out.println(vect[0]);
+        System.out.println(vect[1]);
+        System.out.println(vect[2]);
+        System.out.println(vect[3]);
+        System.out.println(vect[4]);
+        System.out.println(vect[5]);
+        System.out.println(vect[6]);
+        System.out.println(vect[7]);
+
+    }
+}
